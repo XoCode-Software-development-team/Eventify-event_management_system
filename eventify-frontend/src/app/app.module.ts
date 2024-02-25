933c8dd8e8df15940c3b2346ac3213cab7f6d178
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2024-02-25 13:29:46 +0530

    create sort component

--- eventify-frontend/src/app/app.module.ts
+++ eventify-frontend/src/app/app.module.ts
@@ -29,12 +29,13 @@ import { ClientServiceComponent } from './Pages/client/client-service/client-ser
 import { SliderComponent } from './Components/slider/slider.component';
 import { CategoryComponent } from './Components/category/category.component';
 import { FilterRatingComponent } from './Components/filter-rating/filter-rating.component';
+import { SortComponent } from './Components/sort/sort.component';
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,SideNavBarComponent, AdminServiceComponent, TabCardComponent, FooterComponent, AdminDeleteRequestComponent, NavIconComponent, VendorServiceComponent, BasicButtonComponent, VendorBookedServicesComponent, VendorBookingRequestsComponent, VendorAddNewServiceComponent, VendorLayoutComponent, AdminLayoutComponent, PrimaryButtonComponent, ClientServiceComponent, SliderComponent, CategoryComponent, FilterRatingComponent
+    AppComponent,SideNavBarComponent, AdminServiceComponent, TabCardComponent, FooterComponent, AdminDeleteRequestComponent, NavIconComponent, VendorServiceComponent, BasicButtonComponent, VendorBookedServicesComponent, VendorBookingRequestsComponent, VendorAddNewServiceComponent, VendorLayoutComponent, AdminLayoutComponent, PrimaryButtonComponent, ClientServiceComponent, SliderComponent, CategoryComponent, FilterRatingComponent, SortComponent
     ],
   imports: [
     BrowserModule,
