5265cd4396f8bd5b70836710ecc92da8a1d74b9b
Deepthi rasad <drdharmarathna123@gmail.com>
2024-03-16 12:20:20 +0530

    Add event create form component

---
+++ src/app/Components/event-create-form/event-create-form.component.ts
@@ -0,0 +1,149 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Location } from '@angular/common';
+
+
+@Component({
+  selector: 'app-event-create-form',
+  templateUrl: './event-create-form.component.html',
+  styleUrls: ['./event-create-form.component.scss']
+})
+export class EventCreateFormComponent implements OnInit{
+
+  
+  form: FormGroup;
+  eventArray: any[] = [];
+  isResultLoaded = false;
+  isUpdateFormActive = false;
+  currentEventID = '';
+
+  constructor(private http: HttpClient, private fb: FormBuilder, private router: Router, private location: Location) {
+    this.form = this.fb.group({
+      eventName: ['', Validators.required],       //edit
+      description: ['', Validators.required],
+      eventDate: ['', Validators.required],
+      location: ['', Validators.required],
+      startTime: ['', Validators.required],
+      endTime: ['', Validators.required],
+      guestCount: ['', Validators.required],
+      coverImage: ['']
+    });
+  }
+
+  ngOnInit(): void {
+    this.getAllEvent();
+  }
+  getAllEvent() {
+    this.http.get("https://localhost:7128/api/Event/GetEvent")
+      .subscribe((resultData: any) => {
+        this.isResultLoaded = true;
+        console.log(resultData);
+        this.eventArray = resultData;
+      });
+  }
+
+  register() {
+    if (this.form.valid) {
+      let bodyData = {
+        "eventName": this.form.value.eventName,    //edit
+        "description": this.form.value.description,
+        "eventDate": this.form.value.eventDate,
+        "location": this.form.value.location,
+        "startTime": this.form.value.startTime,
+        "endTime": this.form.value.startTime,
+        "guestCount": this.form.value.guestCount,
+        "coverImage":this.form.value.coverImage
+      };
+
+      this.http.post("https://localhost:7128/api/Event/AddEvent", bodyData).subscribe((resultData: any) => {
+        console.log(resultData);
+        alert("Event Registered Successfully")
+        const newEventId = resultData.id;
+        this.router.navigate(['/view', newEventId]);
+        this.getAllEvent();
+        this.form.reset();
+        this.location.go(this.location.path());
+      });
+
+    }else {
+      alert("Form is not valid. Please check your inputs.");
+  }
+
+    
+  }
+
+//-----------------------------------------------------------------------------------------
+
+  setUpdate(data: any) {
+    this.form.patchValue({
+      eventName: data.eventName,             //edit
+      description: data.description,
+      eventDate: data.eventDate, 
+      location: data.location, 
+      startTime: data.startTime, 
+      endTime: data.endTime, 
+      guestCount: data.guestCount, 
+    });
+    this.currentEventID = data.id;
+    this.isUpdateFormActive = true;
+  }
+
+  
+  updateRecord() {
+    if (this.form.valid) {
+      let bodyData = {
+        "eventName": this.form.value.eventName,    //edit
+        "description": this.form.value.description,
+        "eventDate": this.form.value.eventDate,
+        "location": this.form.value.location,
+        "startTime": this.form.value.startTime,
+        "endTime": this.form.value.startTime,
+        "guestCount": this.form.value.guestCount
+      };
+  
+      this.http.patch("https://localhost:7128/api/Event/UpdateEvent/" + this.currentEventID, bodyData)
+        .subscribe((resultData: any) => {
+          console.log(resultData);
+          alert("Event Updated Successfully");
+          this.getAllEvent();
+          this.form.reset();
+          this.isUpdateFormActive = false;
+        }, (error) => {
+          console.error("Error updating event:", error);
+          alert("Failed to update event. Please try again.");
+        });
+    }
+  }
+
+  save() {
+    if (this.isUpdateFormActive) {
+      this.updateRecord();
+    } else {
+      this.register();
+    }
+  }
+
+  
+  imageUrl: any;
+  onFileSelected(event: any) {
+      const file: File = event.target.files[0];
+      if (file) {
+          const reader = new FileReader();
+          reader.onload = (e: any) => {
+              this.imageUrl = e.target.result;
+          };
+          reader.readAsDataURL(file);
+      }
+      
+  }
+
+
+  isInvalid(controlName: string): boolean {
+    const control = this.form.get(controlName);
+    return !!control && control.invalid && (control.touched || control.dirty);
+}
+
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,34 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+//Other
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+//MAT
 import {MatIconModule} from '@angular/material/icon';
+import { MatDividerModule } from '@angular/material/divider';
+import { EventCreateFormComponent } from './Components/event-create-form/event-create-form.component';
+import { HttpClientModule } from '@angular/common/http';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventCreateFormComponent
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatIconModule
+    //MAT
+    MatIconModule,
+    MatDividerModule,
+    //Other
+    HttpClientModule,
+    ReactiveFormsModule,
+    FormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
