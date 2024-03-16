4f3aefaeb1589f3c454ccc06bc5792d0880df10e
Deepthi rasad <drdharmarathna123@gmail.com>
2024-03-16 16:11:46 +0530

    Update CSS styles in components
    Add drop-down list
    Page alignment
    Add icon assets

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EventCreateFormComponent } from './Components/event-create-form/event-create-form.component';
+import { EventViewComponent } from './Components/event-view/event-view.component';
 
 const routes: Routes = [
+  { path: 'create', component: EventCreateFormComponent},
+  { path: 'view/:id', component: EventViewComponent },
+  { path: '', redirectTo: '/create', pathMatch: 'full' },
 ];
 
 @NgModule({
