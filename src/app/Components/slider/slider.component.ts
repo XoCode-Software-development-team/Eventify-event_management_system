869b936687f233a14b6fe6a8e9c54b4fe93a01d8
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2024-04-04 11:31:40 +0530

    modify event card

--- src/app/Components/slider/slider.component.ts
+++ src/app/Components/slider/slider.component.ts
@@ -1,15 +1,22 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
   templateUrl: './slider.component.html',
   styleUrls: ['./slider.component.scss']
 })
-export class SliderComponent {
+export class SliderComponent implements OnInit {
   @Input() maxPrice:number | undefined;
+  max = 0;
+
+  ngOnInit(): void {
+    if (this.maxPrice != null) {
+      this.max = this.maxPrice;
+    }
+  }
 
   calculateStep(maxPrice:any) {
     return Math.ceil(maxPrice / 100);
-}
+  }
 
 }
