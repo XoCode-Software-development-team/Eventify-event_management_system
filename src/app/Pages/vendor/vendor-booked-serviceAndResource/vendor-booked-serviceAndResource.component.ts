616acabb8057a2f708cbaa2a6ed7e6e8f19c413a
manojtharindu11 <thilakarathnawatm.21@uom.lk>
2024-05-17 11:45:41 +0530

    create booked resources view

--- src/app/Pages/vendor/vendor-booked-serviceAndResource/vendor-booked-serviceAndResource.component.ts
+++ src/app/Pages/vendor/vendor-booked-serviceAndResource/vendor-booked-serviceAndResource.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Category } from 'src/app/Interfaces/interfaces';
+import { CapitalizePipe } from 'src/app/Pipes/Capitalize.pipe';
 import { ServiceAndResourceService } from 'src/app/Services/serviceAndResource/serviceAndResource.service';
 
 @Component({
@@ -8,13 +9,14 @@ import { ServiceAndResourceService } from 'src/app/Services/serviceAndResource/s
   styleUrls: ['./vendor-booked-serviceAndResource.component.scss'],
 })
 export default class VendorBookedServiceAndResourceComponent implements OnInit {
-
-  constructor(private _serviceAndResource: ServiceAndResourceService) { }
+  constructor(private _serviceAndResource: ServiceAndResourceService) {}
 
   noData: boolean = false;
   dataSource = []; // Data source for the table
 
-  categories: Category[] = []; // Array to store service categories
+  categories: Category[] = []; // Array to store service/resource categories
+
+  capitalizedTag = new CapitalizePipe().transform(this.checkUrlString()); //Capitalize text
 
   ngOnInit(): void {
     this.getCategories(); // Fetch categories on component initialization
@@ -22,51 +24,59 @@ export default class VendorBookedServiceAndResourceComponent implements OnInit {
 
   displayedColumns: string[] = [
     'No',
-    'Service',
+    this.capitalizedTag,
     'Event Date',
     'End Date',
   ]; // Columns to be displayed in the table
 
-  /**
-   * Fetches booked services based on the category ID
-   * @param categoryId The ID of the category
-   */
-  getServices(categoryId: string) {
+  //Fetches booked services/resources based on the category ID
+  getServicesAndResources(categoryId: string) {
     this.noData = false;
-    this._serviceAndResource.getBookedServiceAndResourcesOfVendor(categoryId, this.vendorId).subscribe({
-      next: (res: any) => {
-        this.dataSource = res; // Assigns the fetched data to the data source
-        console.log(res);
-        this.noData = res.length == 0 ? true : false;
-      },
-      error: (err: any) => {
-        this.noData = true;
-        console.log(err);
-      },
-    });
+    this.dataSource = [];
+    this._serviceAndResource
+      .getBookedServiceAndResourcesOfVendor(categoryId, this.vendorId)
+      .subscribe({
+        next: (res: any) => {
+          this.dataSource = res; // Assigns the fetched data to the data source
+          this.noData = res.length == 0 ? true : false;
+        },
+        error: (err: any) => {
+          this.noData = true;
+          console.log(err);
+        },
+      });
   }
 
-  vendorId: string = "2a5e7b73-df8e-4b43-b2b1-32a1e82e03ee"; // Vendor ID (temporary)
+  vendorId: string = '2a5e7b73-df8e-4b43-b2b1-32a1e82e03ee'; // Vendor ID (temporary)
 
   /**
-   * Fetches the service categories for booked services
+   * Fetches the service/resource categories for booked services/resources
    */
   getCategories() {
     this.noData = false;
-    this._serviceAndResource.getServiceAndResourceCategoriesOfBookedServices(this.vendorId).subscribe({
-      next: (res: any) => {
-        // Maps the received data to category objects
-        this.categories = res.map((item: any) => ({
-          id: item.categoryId,
-          categoryName: item.serviceCategoryName,
-        }));
-        console.log(res);
-        this.noData = res.length == 0 ? true : false;
-      },
-      error: (err: any) => {
-        console.log(err);
-        this.noData = true;
-      },
-    });
+    this._serviceAndResource
+      .getServiceAndResourceCategoriesOfBookedServices(this.vendorId)
+      .subscribe({
+        next: (res: any) => {
+          // Maps the received data to category objects
+          this.categories = res.map((item: any) => ({
+            id: item.categoryId,
+            categoryName:
+              this.checkUrlString() === 'service'
+                ? item.serviceCategoryName
+                : item.resourceCategoryName,
+          }));
+          this.noData = res.length == 0 ? true : false;
+        },
+        error: (err: any) => {
+          console.log(err);
+          this.noData = true;
+        },
+      });
+  }
+
+  // Identify whether service or resource
+  checkUrlString(): string {
+    return this._serviceAndResource.checkUrlString();
   }
 }
--- src/app/Pages/vendor/vendor-serviceAndResource/vendor-serviceAndResource.component.ts
+++ src/app/Pages/vendor/vendor-serviceAndResource/vendor-serviceAndResource.component.ts
@@ -7,14 +7,12 @@ import { ServiceAndResourceService } from 'src/app/Services/serviceAndResource/s
 @Component({
   selector: 'app-vendor-service',
   templateUrl: './vendor-serviceAndResource.component.html',
-  styleUrls: ['./vendor-serviceAndResource.component.scss']
+  styleUrls: ['./vendor-serviceAndResource.component.scss'],
 })
 export class VendorServiceAndResourceComponent {
   @ViewChild('tabCard') tabCardComponent!: TabCardComponent;
 
-  constructor(
-    private _serviceAndResource: ServiceAndResourceService
-  ) {}
+  constructor(private _serviceAndResource: ServiceAndResourceService) {}
 
   noData: boolean = false;
   dataSource: string[] = [];
@@ -23,34 +21,31 @@ export class VendorServiceAndResourceComponent {
 
   capitalizedTag = new CapitalizePipe().transform(this.checkUrlString()); //Capitalize text
 
-  vendorId: string = "2a5e7b73-df8e-4b43-b2b1-32a1e82e03ee"; // Vendor Id (temporary)
-    // vendorId: string = "b0ae24d4-03a5-4a3e-83b3-2e9c7f3245db";
-
+  vendorId: string = '2a5e7b73-df8e-4b43-b2b1-32a1e82e03ee'; // Vendor Id (temporary)
+  // vendorId: string = "b0ae24d4-03a5-4a3e-83b3-2e9c7f3245db";
 
   ngOnInit(): void {
     this.getCategories(this.vendorId);
   }
 
-  displayedColumns: string[] = [
-    'No',
-    `${this.capitalizedTag}`,
-    'Action',
-  ];
+  displayedColumns: string[] = ['No', `${this.capitalizedTag}`, 'Action'];
 
   // Function to retrieve services/resources based on category
   async getServicesAndResources(categoryId: string) {
     this.noData = false;
     this.dataSource = [];
-    this._serviceAndResource.getVendorServiceAndResourceListByCategory(categoryId,this.vendorId).subscribe({
-      next: (res: any) => {
-        this.dataSource = res;
-        this.noData = res.length == 0 ? true : false;
-      },
-      error: (err: any) => {
-        this.noData = true;
-        console.log(err);
-      },
-    });
+    this._serviceAndResource
+      .getVendorServiceAndResourceListByCategory(categoryId, this.vendorId)
+      .subscribe({
+        next: (res: any) => {
+          this.dataSource = res;
+          this.noData = res.length == 0 ? true : false;
+        },
+        error: (err: any) => {
+          this.noData = true;
+          console.log(err);
+        },
+      });
   }
 
   // Function to retrieve categories
@@ -58,12 +53,13 @@ export class VendorServiceAndResourceComponent {
     this.noData = false;
     this._serviceAndResource.getCategoriesListByVendor(id).subscribe({
       next: (res: any) => {
-        this.categories = res.map((item:any) => ({
+        this.categories = res.map((item: any) => ({
           id: item.categoryId,
-          categoryName: this.checkUrlString() === 'service'
-          ? item.serviceCategoryName
-          : item.resourceCategoryName,
-        }))
+          categoryName:
+            this.checkUrlString() === 'service'
+              ? item.serviceCategoryName
+              : item.resourceCategoryName,
+        }));
         this.noData = res.length == 0 ? true : false;
       },
       error: (err: any) => {
@@ -78,11 +74,11 @@ export class VendorServiceAndResourceComponent {
     this._serviceAndResource.RequestToDelete(id).subscribe({
       next: (res: any) => {
         if (deleteRequest) {
-          alert("Cancel the delete request")
+          alert('Cancel the delete request');
         } else {
-          alert("Send delete request to admin");
+          alert('Send delete request to admin');
         }
-        console.log(id,res)
+        console.log(id, res);
         this.getServicesAndResources(res);
       },
       error: (err: any) => {
@@ -91,8 +87,8 @@ export class VendorServiceAndResourceComponent {
     });
   }
 
-    // Identify whether service or resource
-    checkUrlString(): string {
-      return this._serviceAndResource.checkUrlString();
-    }
+  // Identify whether service or resource
+  checkUrlString(): string {
+    return this._serviceAndResource.checkUrlString();
+  }
 }
