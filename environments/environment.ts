c275130cfa79b15ebc4c12ddb8a17f45526bef36
Tharindu Manoj Thilakarathna <thilakarathnawatm.21@uom.lk>
2024-09-21 01:02:02 +0530

    add .env file

--- environments/environment.ts
+++ environments/environment.ts
@@ -1,21 +1,20 @@
 export const baseApiUrl = {
-    Url: 'https://localhost:7164'
+    Url: process.env.BASE_API_URL
 }
 
 export const environment = {
-    firebaseConfig : {
-        apiKey: "AIzaSyDr2zkeI3t59kS6bjDIieH7wUWSw62IAdk",
-        authDomain: "eventify-45015.firebaseapp.com",
-        projectId: "eventify-45015",
-        storageBucket: "eventify-45015.appspot.com",
-        messagingSenderId: "539332405785",
-        appId: "1:539332405785:web:3c3d6ad5b95e7758add888",
-        measurementId: "G-0126Z1V2MM"
-      }
+    firebaseConfig: {
+        apiKey: process.env.FIREBASE_API_KEY,
+        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.FIREBASE_APP_ID,
+        measurementId: process.env.FIREBASE_MEASUREMENT_ID
+    }
 }
 
 export const mapEnvironment = {
     production: false,
-    googleMapsApiKey: 'AIzaSyB0sAO-pDp_nivGlGRLjrvN-Mjo4rfcy5Q'
-  };
-  
+    googleMapsApiKey: process.env.GOOGLE_MAPS_API_KEY
+};
